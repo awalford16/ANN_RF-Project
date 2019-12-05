620261f74469720b8fee0c0d4e8ac728b1eeb7ea
Adam Walford <adamwalford@Adams-MacBook-Pro.local>
2019-12-05 16:41:12 +0000

    density plots

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data_processing import Data
-import os
+from plotting import Plot
+import pylab
 
 def main():
     dp = Data('nuclear_plants.csv')
@@ -7,14 +8,18 @@ def main():
     # Normalise data to scale values
     # dp.norm_data()
 
-    print(dp.data.head())
-    print(dp.get_mean())
-    print(dp.get_stan_dev())
-    print(dp.get_min())
-    print(dp.get_max())
-    print(dp.get_median())
-    print(dp.get_missing_value_count())
-    print(dp.get_feature_count())
+    # print(dp.data.head())
+    # print(dp.get_mean())
+    # print(dp.get_stan_dev())
+    # print(dp.get_min())
+    # print(dp.get_max())
+    # print(dp.get_median())
+    # print(dp.get_missing_value_count())
+    # print(dp.get_feature_count())
+
+    bx_plt = Plot()
+    print(bx_plt.data_box_plot(dp.data, 'Status', 'Vibration_sensor_1'), pylab.show())
+    print(bx_plt.data_density_plot(dp.data, 'Status', 'Vibration_sensor_2'), pylab.show())
 
 if __name__ == '__main__':
     main()
---
+++ plotting.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+class Plot:
+    def data_box_plot(self, data, col1, col2):
+        status_data = data.groupby(col1)
+        return status_data.boxplot(column=col2)
+
+    def data_density_plot(self, data, col1, col2):
+        # Group by status and plot based on column2
+        data.groupby(col1)[col2].plot(kind='density', legend=True)
