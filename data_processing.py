6820f737513c1ee9e5f9054bbbb4dfe14849e431
Adam Walford <adamwalford@Adams-MacBook-Pro.local>
2019-12-06 15:10:52 +0000

    Create variance function

--- data_processing.py
+++ data_processing.py
@@ -40,6 +40,10 @@ class Data:
         # Get the number of columns in each observation (Use first observation for column count)
         return self.data.count(axis=1)[0]
 
+    def get_variance(self):
+        # Pandas functionality to get the variation of each column
+        return self.data.var()
+
     def norm_data(self):
         # Ignore data that is not numerical
         data = self.data.select_dtypes(include=[np.number])
