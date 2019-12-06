866e77176dc58fd049c0b22863a75e3aba5c35ab
awalford16 <awalford16@gmail.com>
2019-12-06 19:29:03 +0000

    commenting

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from plotting import Plot
 import pylab
 
 def main():
-    dp = Data('nuclear_plants.csv')
+    # Create an instance of the data class which will store the csv data in a dataframe
+    plant_data = Data('nuclear_plants.csv')
     
     # Normalise data to scale values
-    # dp.norm_data()
+    # plant_data.norm_data()
 
-    # print(f"Mean: \n{dp.get_mean()}")
-    # print(f"Standard Deviation: \n{dp.get_stan_dev()}")
-    # print(f"Minimum: \n{dp.get_min()}")
-    # print(f"Maximum: \n{dp.get_max()}")
-    # print(f"Median: \n{dp.get_median()}")
-    # print(dp.get_missing_value_count())
-    # print(dp.get_feature_count())
-    # print(f"Variance: \n{dp.get_variance()}")
+    # print(f"Mean: \n{plant_data.get_mean()}")
+    # print(f"Standard Deviation: \n{plant_data.get_stan_dev()}")
+    # print(f"Minimum: \n{plant_data.get_min()}")
+    # print(f"Maximum: \n{plant_data.get_max()}")
+    # print(f"Median: \n{plant_data.get_median()}")
+    # print(plant_data.get_missing_value_count())
+    # print(plant_data.get_feature_count())
+    # print(f"Variance: \n{plant_data.get_variance()}")
 
     bx_plt = Plot()
-    print(bx_plt.data_box_plot(dp.data, 'Status', 'Vibration_sensor_1'), pylab.show())
-    print(bx_plt.data_density_plot(dp.data, 'Status', 'Vibration_sensor_2'), pylab.show())
+    print(bx_plt.data_box_plot(plant_data.data, 'Status', 'Vibration_sensor_1'), pylab.show())
+    print(bx_plt.data_density_plot(plant_data.data, 'Status', 'Vibration_sensor_2'), pylab.show())
 
 if __name__ == '__main__':
     main()
--- plotting.py
+++ plotting.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 class Plot:
     def data_box_plot(self, data, col1, col2):
+        # Group the data based on the first column (status)
         status_data = data.groupby(col1)
         return status_data.boxplot(column=col2)
 
     def data_density_plot(self, data, col1, col2):
-        # Group by status and plot based on column2
+        # Group the data and use the data from col2 of each group
         data.groupby(col1)[col2].plot(kind='density', legend=True)
